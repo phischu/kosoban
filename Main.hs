b0cc922a1eb5093d650d920e5031ef8d4f84b19e
phischu <pschuster@uni-koblenz.de>
2014-11-02 11:54:31 +0100

    failing to even get the overall structure right

--- Main.hs
+++ Main.hs
@@ -1,6 +1,41 @@
 module Main where
 
+import Graphics.Gloss.Interface.IO.Game
+
+import Network.Simple.TCP
+
+import Control.Concurrent.STM
+
+import Data.Set (Set,empty)
+
+data Command = Command Time PlayerName Action
+    deriving (Show,Read,Eq,Ord)
+
+type Time = Integer
+
+type PlayerName = String
+
+data Action = East | North | West | South | Connect
+    deriving (Show,Read,Eq,Ord)
+
 main :: IO ()
-main = putStrLn "hallo"
+main = withSocketsDo (do
+    commandsVar <- atomically (newTVar empty)
+    commandVar <- atomically newEmptyTMVar
+    playIO
+        (InWindow "Kosoban" (600,600) (100,200))
+        white
+        20
+        ()
+        (const (fmap render (atomically (readTVar commandVar))))
+        (handle commandVar
+        step)
 
+render :: Set Command -> Picture
+render = undefined
 
+handle :: Event -> () -> IO ()
+handle = undefined
+
+step :: Float -> () -> IO ()
+step = undefined
