0b26b6347f2bca990afea365ff1d44c1a107a2c3
phischu <pschuster@uni-koblenz.de>
2014-11-06 13:03:44 +0100

    more threads

--- Main.hs
+++ Main.hs
@@ -1,52 +1,69 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Graphics.Gloss.Interface.IO.Game
 
 import Network.Simple.TCP (
-    withSocketsDo)
+    withSocketsDo,
+    serve,HostPreference(HostAny))
+
+import Pipes.Network.TCP (toSocket,fromSocket)
 
 import Pipes.Concurrent
 
+import Pipes
+
+import qualified Pipes.Prelude as Pipes (map)
+
+import Data.Binary (Binary,encode,decode)
+import Data.ByteString.Lazy (toStrict,fromStrict)
+
 import Control.Concurrent.STM (
-    newTVarIO,TVar,readTVarIO,readTVar,writeTVar)
+    newTVarIO,TVar,readTVarIO,readTVar,writeTVar,modifyTVar)
 
 import Data.Set (Set,empty,singleton,union)
+import Data.Maybe (fromJust)
+
+import GHC.Generics (Generic)
 
 import Control.Monad (forever,unless)
+import Data.Monoid (mconcat)
 
 data Command = Command Time PlayerName Action
-    deriving (Show,Read,Eq,Ord)
+    deriving (Show,Read,Eq,Ord,Generic)
+
+instance Binary Command
 
 type Time = Integer
 
 type PlayerName = String
 
 data Action = East | North | West | South | Connect
-    deriving (Show,Read,Eq,Ord)
+    deriving (Show,Read,Eq,Ord,Generic)
+
+instance Binary Action
 
 main :: IO ()
 main = withSocketsDo (do
 
     (inboxO,inboxI) <- spawn Single
     (outboxO,outboxI) <- spawn Single
-    commandsVar <- newTVarIO empty
-    connectionVar <- newTVarIO empty
+    commandV <- newTVarIO empty
+    peerV <- newTVarIO []
+    outputV <- newTVarIO [] 
 
-    forkIO (forever (atomically (do
-            Just incomingCommands <- recv inboxI
-            currentCommands <- readTVar commandsVar
-            let newCommands = union incomingCommands currentCommands
-            unless (newCommands == currentCommands) (do
-                writeTVar commandsVar newCommands
-                send outboxO newCommands
-                return ()))))
+    forkIO (updateCommands inboxI outboxO commandV)
+
+    forkIO (serveOutputs outboxI outputV)
+
+    forkIO (acceptConnections peerV outputV inboxO)
 
     playIO
         (InWindow "Kosoban" (600,600) (100,200))
         white
         20
         ()
-        (render commandsVar)
+        (render commandV)
         (handle inboxO)
         (const return))
 
@@ -60,4 +77,27 @@ handle inboxO event () = do
     let command = undefined
     atomically (send inboxO (singleton command))
     return ()
+
+updateCommands :: Input (Set Command) -> Output (Set Command) -> TVar (Set Command) -> IO ()
+updateCommands inboxI outboxO commandV = forever (atomically (do
+    Just incomingCommands <- recv inboxI
+    currentCommands <- readTVar commandV
+    let newCommands = union incomingCommands currentCommands
+    unless (newCommands == currentCommands) (do
+	writeTVar commandV newCommands
+	send outboxO newCommands
+	return ())))
+
+serveOutputs :: Input (Set Command) -> TVar [Output (Set Command)] -> IO ()
+serveOutputs outboxI outputV = forever (do
+    Just commands <- atomically (recv outboxI)
+    outputs <- readTVarIO outputV
+    atomically (send (mconcat outputs) commands))
+
+acceptConnections :: TVar [()] -> TVar [Output (Set Command)] -> Output (Set Command) -> IO ()
+acceptConnections peersV outputsV inboxO = serve HostAny "3894" (\(socket,sockAddr) -> do
+    (connectionO,connectionI) <- spawn Single
+    forkIO (runEffect (fromInput connectionI >-> Pipes.map (toStrict . encode) >-> toSocket socket))
+    forkIO (runEffect (fromSocket socket 4096 >-> Pipes.map (fromJust . decode . fromStrict) >-> toOutput inboxO))
+    atomically (modifyTVar outputsV (connectionO:)))
 
